fe54aff31c31dd8021902fbd9b0eafa3232be099
Tyler Buck <tyler.buck@maine.edu>
2015-02-24 19:58:46 -0500

    Add cacao to bower file and update import paths

--- Gruntfile.js
+++ Gruntfile.js
@@ -105,13 +105,12 @@ module.exports = function(grunt) {
 
 
     // cacao modules
-    require('./cacao/modules/jquery/Gruntfile.js')(grunt);
-    require('./cacao/modules/magnific/Gruntfile.js')(grunt);
-    require('./cacao/modules/normalize/Gruntfile.js')(grunt);
-    require('./cacao/modules/slick/Gruntfile.js')(grunt);
-    require('./cacao/modules/slider/Gruntfile.js')(grunt);
-    require('./cacao/modules/mainnav/Gruntfile.js')(grunt);
-    require('./cacao/modules/ddmenu/Gruntfile.js')(grunt);
+    //require('./bower_components/cacao/modules/jquery/Gruntfile.js')(grunt);
+    //require('./bower_components/cacao/modules/magnific/Gruntfile.js')(grunt);
+    require('./bower_components/cacao/modules/normalize/Gruntfile.js')(grunt);
+    //require('./bower_components/cacao/modules/slick/Gruntfile.js')(grunt);
+    require('./bower_components/cacao/modules/slider/Gruntfile.js')(grunt);
+    //require('./bower_components/cacao/modules/ddmenu/Gruntfile.js')(grunt);
 
 
     // npm tasks
