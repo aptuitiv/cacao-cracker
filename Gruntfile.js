93e8d4650be91983ae09b01c0c4d154314585d6f
Tyler Buck <tyler.buck@maine.edu>
2015-03-13 11:03:09 -0400

    Fix image destination directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,7 +38,7 @@ module.exports = function(grunt) {
                     expand: true,
                     cwd: '<%= global.site %>/assets',
                     src: ['**/*.{png,jpg,gif}'],
-                    dest: '<%= global.dest %>/images'
+                    dest: '<%= global.dest %>/layout/images'
                 }]
             }
         },
